9e5c6edf4add4315d6de34dc8e84ab7472354c0b
KaueWallace <kauewall8@gmail.com>
2026-06-10 16:22:53 -0400

    atualizando endpoint'

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 
 export const environment = {
   production: true,
-  baseUrl:'https://apiadmin.tecnocomp.cloud',
+  baseUrl:'https://apiadminproex.tecnocomp.cloud',
 };
 
 export const environmentFrontEnd = {
   production: true,
-  baseUrl: 'https://apiadmin.tecnocomp.cloud'
+  baseUrl: 'https://apiadminproex.tecnocomp.cloud'
 }
