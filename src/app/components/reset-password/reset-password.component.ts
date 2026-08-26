c1a26ac2677c1459b79b38a21ebf6b3db23a729c
KaueWallace <kauewall8@gmail.com>
2026-08-26 15:41:03 -0400

    estilização e ajustes de validação da tela de redefinir senha

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiAdmService } from 'src/app/services/api-adm.service';
 import { OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { noOnlyWhitespace, senhaForte } from '../validators/validators';
 
 @Component({
   selector: 'app-reset-password',
@@ -13,9 +14,15 @@ export class ResetPasswordComponent implements OnInit {
   email!: string;
   token!: string; 
 
+  errorLogin: boolean = false;
+  submitted = false;
+
+  hide = true;
+  hideDois = true
+
   redefinirSenha = new FormGroup({
-    nova_senha: new FormControl(''),
-    confirmar_senha: new FormControl(''),
+    nova_senha: new FormControl('', [Validators.required, Validators.minLength(8), senhaForte(), noOnlyWhitespace()]),
+    confirmar_senha: new FormControl('', [Validators.required]),
   });
 
   constructor(
@@ -30,24 +37,38 @@ export class ResetPasswordComponent implements OnInit {
   } 
 
   submit(){
-      if (this.redefinirSenha.value.nova_senha == this.redefinirSenha.value.confirmar_senha){
+    this.submitted = true;
+      if (this.isSenhaIgual && this.redefinirSenha.valid){
           this.ApiAdmService.resetPassword(
             this.email,
             this.token, 
             this.redefinirSenha.value.nova_senha!
           ).subscribe({
             next: () => {
+              this.errorLogin = false;
+              
+              // esvazia os campos do formulário
+              this.redefinirSenha.reset()
+              
               this.route.navigate(['/login'])
+              
               this.ApiAdmService.message('Senha redefinida com sucesso')
             },
             error: () => {
+              this.errorLogin = true;
               this.route.navigate(['/login'])
               this.ApiAdmService.message('Não foi possível redefinir a senha. O link pode ter expirado!')
             }
           })
       }
-      // esvazia os campos do formulário
-      this.redefinirSenha.reset()
+        if (this.redefinirSenha.invalid || !this.isSenhaIgual){
+          this.errorLogin = true;
+        }
+
   }
   
+
+  get isSenhaIgual(){
+    return this.redefinirSenha.value.nova_senha === this.redefinirSenha.value.confirmar_senha;
+  }
 }
